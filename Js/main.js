3603e01236664821db4bf80ebf2cf310b9dcab55
thesecretcode01 <thesecretcode0001@gmail.com>
2023-05-27 20:57:56 +0600

    Update

---
+++ Js/main.js
@@ -0,0 +1,92 @@
+
+window.addEventListener('scroll', {
+    scroll_animations,
+});
+
+
+
+
+
+function scroll_animations() {
+    // var allow_on_mobile = !0;
+    // if (typeof config_scroll_animation_on_mobile !== "undefined") allow_on_mobile = config_scroll_animation_on_mobile;
+    // if (allow_on_mobile == !1 && is_mobile_device) return;
+    var defaults = {
+        duration: 1.2,
+        ease: "power4.out",
+        animation: "fade_from_bottom",
+        once: !1,
+    };
+    gsap.utils.toArray(".scroll-animation").forEach(function(box) {
+        var gsap_obj = {};
+        var settings = {
+            // ease: box.dataset.animationEase || defaults.ease,
+            duration: box.dataset.animationDuration || defaults.duration,
+        };
+        var animations = {
+            fade_from_bottom: {
+                y: 180,
+                opacity: 0,
+            },
+            fade_from_top: {
+                y: -180,
+                opacity: 0,
+            },
+            fade_from_left: {
+                x: -180,
+                opacity: 0,
+            },
+            fade_from_right: {
+                x: 180,
+                opacity: 0,
+            },
+            fade_in: {
+                opacity: 0,
+            },
+            rotate_up: {
+                y: 180,
+                rotation: 10,
+                opacity: 0,
+            },
+        };
+        var scroll_trigger = {
+            scrollTrigger: {
+                trigger: box,
+                once: defaults.once,
+                start: "top bottom+=20%",
+                // start: "top bottom+=5%",
+                toggleActions: "play none none reverse",
+                markers: !1,
+            },
+        };
+        jQuery.extend(gsap_obj, settings);
+        jQuery.extend(gsap_obj, animations[box.dataset.animation || defaults.animation]);
+        jQuery.extend(gsap_obj, scroll_trigger);
+        gsap.from(box, gsap_obj);
+    });
+}
+scroll_animations();
+
+
+
+
+
+
+
+/**
+ * go top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  if (window.scrollY >= 800) {
+    goTopBtn.classList.add("active");
+  } else {
+    goTopBtn.classList.remove("active");
+  }
+
+})
+
+     
